fc50fc3e5db7a34f8ccb92311980316b0def26b1
Rus Hughes <russell.hughes@gmail.com>
2019-06-22 17:45:18 +0200

    adding debug logging

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.user = current_user
+    logger.debug @article
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
