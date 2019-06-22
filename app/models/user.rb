b066df000563f7746d7d3bc00ece8d8020205a36
Rus Hughes <russell.hughes@gmail.com>
2019-06-22 18:21:23 +0200

    s6 l143

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save {self.email = email.downcase }
   validates :username, presence: true, uniqueness: {case_sensitive: false},
             length:  { minimum: 3, maximum: 25 }
@@ -9,6 +9,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
-            
+
   has_secure_password
 end
