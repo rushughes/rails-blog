a77971618181c10d213ba5e999106b4d41e9e1d8
Rus Hughes <russell.hughes@gmail.com>
2019-06-22 21:13:01 +0200

    s7 l152

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -13,4 +13,21 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_match "sports", response.body
   end
 
+  test "invalid category submission results in failure" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: "" } }
+      post categories_path, params: { category: { name: " " } }
+      post categories_path, params: { category: { name: "  " } }
+      post categories_path, params: { category: { name: "   " } }
+      post categories_path, params: { category: { name: "a" } }
+      post categories_path, params: { category: { name: "aa" } }
+      post categories_path, params: { category: { name: "a" * 30 } }
+    end
+    assert_template 'categories/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+
 end
